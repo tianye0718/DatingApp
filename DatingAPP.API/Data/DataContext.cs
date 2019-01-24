b762ae972f635fb5d892d5b1d4bd2860cd208bc9
Ye Tian <Ye.Tian@frontiersoftware.co.nz>
2019-01-24 15:31:20 +1300

    Initial Commit

---
+++ DatingAPP.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingAPP.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPP.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
