65e97fb738986a75d3948a81f009c4ad0b1935c0
Ye Tian <Ye.Tian@frontiersoftware.co.nz>
2019-01-29 23:18:22 +1300

    End of Section 12 - Reactive Forms

--- DatingAPP.API/Helpers/AutoMapperProfiles.cs
+++ DatingAPP.API/Helpers/AutoMapperProfiles.cs
@@ -27,6 +27,7 @@ namespace DatingAPP.API.Helpers
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
